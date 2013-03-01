a3da4742fe672d4ee81da861fb5dc3049e42dea7
28kayak <kaya_ota@msn.com>
2013-02-28 23:10:45 -0800

    modify submit file

--- COncertSubmit.cpp
+++ COncertSubmit.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <cstring>
 #include<cstdlib>
+#include<cstdio>
 using namespace std;
 //function list
 bool soldOut(int seats[], int size);//determines if all  seats are sold
@@ -36,54 +37,70 @@ int main()
         seatNum = 0;
         do
         {
-		 cout << "0: Exist this program.\n"
-	              << "1: Check if seats are sold out.\n" 
-            	      << "2: Reserve a seat.\n" 
-                      << "3: Find the furthest seat available.\n" 
-                      << "4: Find seats that are next to each other.\n"
-                      << "5: Cancel a reserved seat.\n" 
-                      << "6: Print out the number of seats left.\n" 
-                      << "7:Print out the status of all seats.\n";
-       		 cin >> action;
+	         cout << "1: Check if seats are sold out.\n";
+            	 cout << "2: Reserve a seat.\n";
+                 cout << "3: Cancel a reserved seat.\n" ;
+                 cout << "4: Find seats that are next to each other.\n";
+                 cout << "5: Find the furthest seat available.\n";
+                 cout << "6: Print out the number of seats left.\n"; 
+                 cout << "7: Print out the status of all seats.\n";
+       		 cout << "99:Exit the program\n"; 		 
+		 scanf("%d", &action); 
+		if(isdigit(action))
+		 {
+			cout << "this is not digit\n";
+		 }
 		 switch(action)
 	         {
-		case 0:
-      			cout << "Exit from this program";
-       			cout << "Thank you for using!";
-       			cout << "Programmed by Kaya Ota";		       
-		case 1:
-	               	if(soldOut(seats,MAXSEATS))
-               		 {
-              			 cout << "This concert is already sold out.\n";
-			 }
-			 else
-			 {
-		                 cout << "There are still seats available. ASAP to reserve!\n";
-			 }
+			case 99:
+      				cout << "Exit from this program\n";
+       				cout << "Thank you for using!\n";
+	       			cout << "Programmed by Kaya Ota\n";
+				break;		       
+			case 1:
+		               	if(soldOut(seats,MAXSEATS))
+	               		 {
+	              			 cout << "This concert is already sold out.\n";
+				 }
+				 else
+				 {
+			                 cout << "There are still seats available.\n ASAP to reserve!\n";
+				 }
 				break;
-           	case 2://reserve seat
-     			 while(!getSeatNo(seatNum) || !validSeatNum(seatNum,MAXSEATS));
-               		 reserveSeat(seatNum,seats, MAXSEATS);
-                	break;
-		case 3://cancel seat
-               		while(!getSeatNo(seatNum) || !validSeatNum(seatNum, MAXSEATS));
-               		cancelSeat(seatNum, seats, MAXSEATS);
-            		break;
-		case 4:///find pairSeat
-                	pairSeat(seats, MAXSEATS);
-                	break;
-		case 5://find the furthest seat
-                	cout <<"Find the furthest seats from the front";
-               		furthestFront(seats, MAXSEATS);
-               		break;
-           	case 6:
-                	cout << seatsLeft(seats, MAXSEATS) <<" seats are left.";
-                	break;
-		case 7:
-                	showStatus(seats,MAXSEATS);
-               		break;
-		}//switch
-        }while(action != 0);//while
+        	   	case 2://reserve seat
+     				 while(!getSeatNo(seatNum) || !validSeatNum(seatNum,MAXSEATS));
+               			 reserveSeat(seatNum,seats, MAXSEATS);
+                		break;
+			case 3://cancel seat
+               			while(!getSeatNo(seatNum) || !validSeatNum(seatNum, MAXSEATS));
+               			if(cancelSeat(seatNum, seats, MAXSEATS))
+				{
+					cout << "Canceled";
+				}
+				else
+				{
+					cout << "seat may not be reserved or cancel is faile";
+				}
+            			break;
+			case 4:///find pairSeat
+                		pairSeat(seats, MAXSEATS);
+                		break;
+			case 5://find the furthest seat
+	                	cout <<"Find the furthest seats from the front\n";
+	               		furthestFront(seats, MAXSEATS);
+	               		break;
+	           	case 6:
+        	        	cout << seatsLeft(seats, MAXSEATS) <<" seats are left.\n";
+        	        	break;
+			case 7:
+                		showStatus(seats,MAXSEATS);
+               			break;
+			default :
+				cout << "invalid action number, please choose another\n";
+				break;
+				
+		}//switch	
+        }while(action != 99);//while
 	return 0;
 }
 void showStatus(int seats[], int size)
@@ -100,7 +117,7 @@ void showStatus(int seats[], int size)
         }
         else
         {
-            cout << "element is invalid";
+            cout << "element is invalid\n";
         }
      }//for
 }//show status
@@ -124,7 +141,7 @@ bool soldOut(int seats[], int size)
 int seatsLeft(int seats[], int size)
 {
       int available= 0;        
-      for(int index = 0; index < size; index++)
+      for(int index = 1; index < size; index++)
       {
           if(seats[index] == 0)
           {
@@ -171,7 +188,7 @@ bool reserveSeat(int seatNo, int seats[], int size)
         }
         else
         {
-		cout << " seat number is sold out\n"; 
+		cout << "seat number is sold out\n"; 
                 reservation = false;
         }        
         return reservation; 
@@ -179,13 +196,10 @@ bool reserveSeat(int seatNo, int seats[], int size)
 bool cancelSeat(int seatNo, int seats[], int size)
 {
       bool cancelSeat = true;
-      cout << "Seat Number Test 1 : " << seatNo << "\n";
       if(seats[seatNo] == 1)
       {
           seats[seatNo] = 0;
           cancelSeat = true;
-          cout << "Seat Number Test 2 : " << seatNo;
-          cout << "\n";
           cout << seats[seatNo];
       }
       else
@@ -205,7 +219,7 @@ int furthestFront(int seats[], int size)
               furthest = index;
           }
       }
-      cout << furthest << "is an available seat from the front";
+      cout << furthest << "is an available seat from the front\n";
 }
 bool getSeatNo(int& value)
 {
@@ -237,7 +251,7 @@ void pairSeat(int seats[], int size)
 	{
 		if(seats[index]==0 && seats[index+1] == 0)
 		{
-			cout << index  << "&" << index+1 <<" are available for pair seats";
+			cout << index  << "&" << index+1 <<" are available for pair seats\n";
 		}
 	}
 } 
